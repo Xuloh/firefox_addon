e97132beef9968fd5851ee8edf0fcae202327634
Pacôme Lehoux <pacome.lehoux@laposte.net>
2017-11-25 16:26:29 +0000

    Modified input to handle directories and modified script to use object urls

--- popup/popup.js
+++ popup/popup.js
@@ -2,4 +2,6 @@ console.log("imotep");
 var folder = document.getElementById("folder");
 folder.addEventListener("input", function() {
     console.log(this.files);
+    for(var i = 0; i < this.files.length; i++)
+        console.log(window.URL.createObjectURL(this.files[i]));
 })
