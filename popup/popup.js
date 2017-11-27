7292f08e6aaa74915fb84f65a6cf9bcadbd0f9d2
Pacôme Lehoux <pacome.lehoux@laposte.net>
2017-11-27 16:18:48 +0000

    Added a background page to play the audio file in the background

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,12 @@
-var audio_player = document.getElementById("audio-player");
-var folder = document.getElementById("folder");
-folder.addEventListener("input", function() {
-    var file = this.files[0];
-    if(file.type.startsWith("audio/")) {
-        var url = window.URL.createObjectURL(file);
-        audio_player.src = url;
-        audio_player.play();
-    }
-})
+browser.runtime.getBackgroundPage().then(function(background_page) {
+    var audio_player = background_page.document.getElementById("audio-player");
+    var folder = document.getElementById("folder");
+    folder.addEventListener("input", function() {
+        var file = this.files[0];
+        if(file.type.startsWith("audio/")) {
+            var url = window.URL.createObjectURL(file);
+            audio_player.src = url;
+            audio_player.play();
+        }
+    });
+});
