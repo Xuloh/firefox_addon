9b25fb57a88a5bb61b87cab5598825ed3ed3c852
Pacôme Lehoux <pacome.lehoux@laposte.net>
2017-11-28 17:58:06 +0000

    Added volume features

--- popup/popup.js
+++ popup/popup.js
@@ -6,6 +6,10 @@ browser.runtime.getBackgroundPage().then(function(background_page) {
     var play_button = document.getElementById("play-button");
     var pause_button = document.getElementById("pause-button");
 
+    var volume_up_button = document.getElementById("volume-up-button");
+    var volume_down_button = document.getElementById("volume-down-button");
+    var mute_button = document.getElementById("mute-button");
+
     file_button.addEventListener("click", function() {
         file_input.click();
     });
@@ -16,5 +20,19 @@ browser.runtime.getBackgroundPage().then(function(background_page) {
 
     pause_button.addEventListener("click", function() {
         audio_player.pause();
+    });
+
+    volume_up_button.addEventListener("click", function() {
+        if(audio_player.volume + 0.1 <= 1.0)
+            audio_player.volume += 0.1;
+    });
+
+    volume_down_button.addEventListener("click", function() {
+        if(audio_player.volume - 0.1 >= 0)
+            audio_player.volume -= 0.1;
+    });
+
+    mute_button.addEventListener("click", function() {
+        audio_player.muted = !audio_player.muted;
     });
 });
