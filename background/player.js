d3a9b60c94a97051a8ab1911bcf2c4f1071cafef
Pacôme Lehoux <pacome.lehoux@laposte.net>
2017-11-29 13:56:51 +0000

    Modified audio player ended event listener

--- background/player.js
+++ background/player.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var file_input = document.getElementById("file-input");
 
     audio_player.addEventListener("ended", function() {
+        URL.revokeObjectURL(this.src);
+        this.src = "";
         now_playing = null;
     });
-    
+
     file_input.addEventListener("input", function() {
         var file = this.files[0];
         if(file.type.startsWith("audio/")) {
-            var url = window.URL.createObjectURL(file);
+            var url = URL.createObjectURL(file);
             audio_player.src = url;
             audio_player.play();
             now_playing = file.name;
