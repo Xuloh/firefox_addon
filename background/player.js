2109bcfa51ce375d42b8ee0f3e80c8535697bc18
Pacôme Lehoux <pacome.lehoux@laposte.net>
2017-11-30 14:46:21 +0000

    Changed script tags position

--- background/player.js
+++ background/player.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var audio_player = document.getElementById("audio-player");
-    var file_input = document.getElementById("file-input");
+var audio_player = document.getElementById("audio-player");
+var file_input = document.getElementById("file-input");
 
-    audio_player.addEventListener("ended", function() {
-        URL.revokeObjectURL(this.src);
-        this.src = "";
-        now_playing = null;
-    });
+audio_player.addEventListener("ended", function() {
+    URL.revokeObjectURL(this.src);
+    this.src = "";
+    now_playing = null;
+});
 
-    file_input.addEventListener("input", function() {
-        var file = this.files[0];
-        if(file.type.startsWith("audio/")) {
-            var url = URL.createObjectURL(file);
-            audio_player.src = url;
-            audio_player.play();
-            now_playing = file.name;
-        }
-    });
+file_input.addEventListener("input", function() {
+    var file = this.files[0];
+    if(file.type.startsWith("audio/")) {
+        var url = URL.createObjectURL(file);
+        audio_player.src = url;
+        audio_player.play();
+        now_playing = file.name;
+    }
 });
 
 var now_playing = null;
